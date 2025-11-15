8b303f2657ed270ca38dddfbc6e8c35ff5d3e4bb
LeonardoTheGood <leonardo.cofone5@gmail.com>
2025-11-15 18:43:52 +0100

    agg14

--- pwa/icon/piii.py
+++ pwa/icon/piii.py
@@ -1,22 +1,19 @@
 from PIL import Image
 
-img = Image.open("Lerri_wbg.png").convert("RGBA")
-pixels = img.load()
-width, height = img.size
-cx, cy = width // 2, height // 2
-radius = min(cx, cy)
-border = 7  # numero di pixel da aggiungere per evitare il bordo bianco
+img = Image.open("SITO/pwa/icon/2.png").convert("RGBA")
 
-for y in range(height):
-    for x in range(width):
-        r, g, b, a = pixels[x, y]
-        dx, dy = x - cx, y - cy
-        if a == 0 and dx*dx + dy*dy < (radius-border)*(radius-border):
-            pixels[x, y] = (255, 255, 255, 255)
+datas = img.getdata()
+new_data = []
+for r, g, b, a in datas:
+    if r > 250 and g > 250 and b > 250:
+        new_data.append((255, 255, 255, 0))
+    else:
+        new_data.append((r, g, b, a))
+img.putdata(new_data)
 
-img.save("logo_outer_white.png")
+img.save("SITO/pwa/icon/LOGO_LERRI.png")
 
 icon_sizes = [(16,16),(32,32),(48,48),(64,64),(128,128),(256,256)]
-img.save("favicon.ico", format="ICO", sizes=icon_sizes)
-img.resize((512,512), Image.Resampling.LANCZOS).save("icon-512.png")
-img.resize((192,192), Image.Resampling.LANCZOS).save("icon-192.png")
+img.save("SITO/pwa/icon/favicon.ico", format="ICO", sizes=icon_sizes)
+img.resize((512,512), Image.Resampling.LANCZOS).save("SITO/pwa/icon/icon-512.png")
+img.resize((192,192), Image.Resampling.LANCZOS).save("SITO/pwa/icon/icon-192.png")
